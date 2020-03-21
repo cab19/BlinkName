76b86ae65e5e244479ff2296be33fef9e988f9fe
Chris Bounsall <cbounsal@deakin.edu.au>
2020-03-22 10:29:36 +1100

    Initial program commit

---
+++ src/Task2-1.ino
@@ -0,0 +1,29 @@
+/*
+ * Project Task2-1
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+<<<<<<< HEAD
+void blinkLED(bool longBlink)
+{
+    int intDelay = longBlink ? 500 : 100; // if long blink set delay to half a second otherwise 100 milliseconds
+    digitalWrite(led1, HIGH); // turn it on
+    delay(intDelay); // wait for designated time
+    digitalWrite(led1, LOW); // turn it off
+    delay(500); // wait another half a second
+} 
+=======
+}
+>>>>>>> parent of 07c3522... Initial program
